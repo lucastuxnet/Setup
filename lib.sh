ada172a37548fe64e4128ca4527321dbeac5391b
Lucas Martins <91044017+lucastuxnet@users.noreply.github.com>
2024-06-25 00:18:31 -0300

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -1,29 +1,5 @@
 #!/bin/bash
 
-# Pergunta se deseja instalar as bibliotecas e aplicações
-read -p "Deseja instalar as bibliotecas e as aplicações? (sim/não): " INSTALL_CONFIRMATION
-
-if [ "$INSTALL_CONFIRMATION" != "sim" ]; then
-    echo "Nenhuma aplicação ou biblioteca foram adicionadas."
-    exit 0
-fi
-
-# Executa a instalação e configuração
-echo "Iniciando a instalação das aplicações e bibliotecas..."
-
-# Atualiza a lista de pacotes e instala as aplicações
-sudo apt-get update && \
-sudo apt install -y podman && \
-sudo apt install -y podman-compose && \
-sudo apt install -y docker.io && \
-sudo usermod -aG docker $USER && \
-sudo chmod 666 /var/run/docker.sock && \
-sudo apt install -y curl && \
-curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64" && \
-sudo mv cosign-linux-amd64 /usr/local/bin/cosign && \
-sudo chmod +x /usr/local/bin/cosign && \
-sudo apt-get install -y jq
-
 # Função para configurar registries.conf
 setup_registries_conf() {
     echo "Configurando registries.conf..."
@@ -48,6 +24,30 @@ EOF
 
 # Configure registries.conf
 setup_registries_conf
+
+# Pergunta se deseja instalar as bibliotecas e aplicações
+read -p "Deseja instalar as bibliotecas e as aplicações? (sim/não): " INSTALL_CONFIRMATION
+
+if [ "$INSTALL_CONFIRMATION" != "sim" ]; then
+    echo "Nenhuma aplicação ou biblioteca foram adicionadas."
+    exit 0
+fi
+
+# Executa a instalação e configuração
+echo "Iniciando a instalação das aplicações e bibliotecas..."
+
+# Atualiza a lista de pacotes e instala as aplicações
+sudo apt-get update && \
+sudo apt install -y podman && \
+sudo apt install -y podman-compose && \
+sudo apt install -y docker.io && \
+sudo usermod -aG docker $USER && \
+sudo chmod 666 /var/run/docker.sock && \
+sudo apt install -y curl && \
+curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64" && \
+sudo mv cosign-linux-amd64 /usr/local/bin/cosign && \
+sudo chmod +x /usr/local/bin/cosign && \
+sudo apt-get install -y jq
 
 # Mensagem de sucesso
 if [ $? -eq 0 ]; then
