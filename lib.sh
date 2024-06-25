0f0bf46bd96976eddc73598b73b0b28aa21b0fd6
Lucas Martins <91044017+lucastuxnet@users.noreply.github.com>
2024-06-24 22:34:08 -0300

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -12,31 +12,36 @@ fi
 echo "Iniciando a instalação das aplicações e bibliotecas..."
 
 # Atualiza a lista de pacotes e instala as aplicações
-sudo apt-get update
-sudo apt install -y podman
-sudo apt install -y python3-pip
-sudo -H pip3 install --upgrade pip
-pip3 install podman-compose
-sudo apt install -y docker.io
-sudo usermod -aG docker $USER
-sudo chmod 666 /var/run/docker.sock
-sudo apt install -y curl
-curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64"
-sudo mv cosign-linux-amd64 /usr/local/bin/cosign
-sudo chmod +x /usr/local/bin/cosign
+sudo apt-get update && \
+sudo apt install -y podman && \
+sudo apt install -y python3-pip && \
+sudo -H pip3 install --upgrade pip && \
+pip3 install podman-compose && \
+sudo apt install -y docker.io && \
+sudo usermod -aG docker $USER && \
+sudo chmod 666 /var/run/docker.sock && \
+sudo apt install -y curl && \
+curl -O -L "https://github.com/sigstore/cosign/releases/latest/download/cosign-linux-amd64" && \
+sudo mv cosign-linux-amd64 /usr/local/bin/cosign && \
+sudo chmod +x /usr/local/bin/cosign && \
 sudo apt-get install -y jq
 
 # Mensagem de sucesso
-echo "Aplicações."
-echo ""
-echo "- Podman"
-echo "- Podman-compose"
-echo "- Docker"
-echo "- Cosign"
-echo "- Pip"
-echo "- JQ"
-echo ""
-echo "Todas as aplicações e bibliotecas foram adicionadas com sucesso."
+if [ $? -eq 0 ]; then
+    echo "Aplicações."
+    echo ""
+    echo "- Podman"
+    echo "- Podman-compose"
+    echo "- Docker"
+    echo "- Cosign"
+    echo "- Pip"
+    echo "- JQ"
+    echo ""
+    echo "Todas as aplicações e bibliotecas foram adicionadas com sucesso."
+else
+    echo "Houve um erro durante a instalação das aplicações e bibliotecas."
+    exit 1
+fi
 
 # Pergunta se deseja criar as chaves de segurança
 read -p "Você quer criar as chaves de segurança? (sim/não): " KEYS_CONFIRMATION
@@ -47,12 +52,16 @@ if [ "$KEYS_CONFIRMATION" != "sim" ]; then
 fi
 
 # Cria a pasta para certificados e navega para ela
-mkdir -p certs
-cd certs
+mkdir -p certs && cd certs
 
 # Gera as chaves de segurança
-cosign generate-key-pair
-podman run -it -v $PWD:/work:z docker.io/library/nginx openssl genrsa -out /work/esolvere_private.pem
+cosign generate-key-pair && \
+podman run -it -v $PWD:/work:z docker.io/library/nginx openssl genrsa -out /work/esolvere_private.pem && \
 podman run -it -v $PWD:/work:z docker.io/library/nginx openssl rsa -in /work/esolvere_private.pem -pubout -out /work/esolvere_public.pem
 
-echo "Chaves de segurança criadas com sucesso."
+if [ $? -eq 0 ]; then
+    echo "Chaves de segurança criadas com sucesso."
+else
+    echo "Houve um erro ao criar as chaves de segurança."
+    exit 1
+fi
