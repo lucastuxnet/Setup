a0a49bc0463d8e02ee9a39606d2bb8af8dabdd59
Lucas Martins <91044017+lucastuxnet@users.noreply.github.com>
2024-06-27 11:53:54 -0300

    Update lib.sh

--- lib.sh
+++ lib.sh
@@ -63,7 +63,7 @@ sudo apt install -y podman && \
 sudo apt install -y podman-compose && \
 sudo apt install -y docker.io && \
 sudo apt install -y openssh-server && \
-sudo apt instal net-tools && \
+sudo apt install net-tools && \
 sudo usermod -aG docker $USER && \
 sudo chmod 666 /var/run/docker.sock && \
 sudo apt install -y curl && \
